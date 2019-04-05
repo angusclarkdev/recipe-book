fc122ad56fa4fd5a5016a900bf0cfc582cffec5e
Angus Clark <angus.c@mmtdigital.co.uk>
2019-04-05 11:22:57 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  devServer: {
+    open: true,
+    port: 8000
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.css|scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoader: 1,
+              modules: true,
+              localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              parser: 'postcss-scss'
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js?$/,
+        loader: 'standard-loader',
+        exclude: /(node_modules)/,
+        options: { parser: 'babel-eslint' }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
